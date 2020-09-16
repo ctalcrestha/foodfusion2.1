395b3f9abed9153869a580cd479bd940f38c4b3b
ctalcrestha <ctalcrestha100Agmail.com>
2020-09-16 22:02:30 +0545

    react component prt 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,47 +1,67 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { Card, CardImg, CardBody, CardTitle, CardText, CardHeader } from 'reactstrap'
 
 
+
+const RenderItem = ({ dish }) => {
+  return (
+
+    <Card>
+      <CardImg top src={dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
+  )
+}
+const RenderComment = ({ dish }) => {
+  return (
+    <div>
+      <h2>Comments</h2>
+      {dish.comments.map(comment => (
+        <ul className="list-unstyled">
+          <li>{comment.comment}</li>
+          <li>{`-- ${comment.author}`}, {new Intl.DateTimeFormat('en-US', { year: "numeric", month: "short", day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
+        </ul>))}
+
+    </div>
+
+
+  )
+}
+
 // initialy -> null
 const DishDetailComponent = (props) => {
- if (props.dishSelected != null){
+  if (props.dishSelected != null) {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                   <Card>
-                         <CardImg top src={props.dishSelected.image} alt={props.dishSelected.name} />
-                         <CardBody>
-                             <CardTitle>{props.dishSelected.name}</CardTitle>
-                               <CardText>{props.dishSelected.description}</CardText>
-                         </CardBody>
-                  </Card>
-                </div> 
-               <div className="co-12 col-md-5 m-1">
-                     <h2>Comments</h2>
-                      {props.dishSelected.comments.map(comment => (
-                    <ul className="list-unstyled">
-                         <li>{comment.comment}</li>
-                         <li>{`-- ${comment.author}`}, {new Intl.DateTimeFormat('en-US',{year:"numeric", month:"short", day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                    </ul> ))}
-               </div>
-       
-            </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderItem dish={props.dishSelected} />
+          </div>
+          <div className="co-12 col-md-5 m-1">
+
+            <RenderComment dish={props.dishSelected} />
+
+          </div>
+
         </div>
-       
-        
+      </div>
+
+
     )
-      
- }
- else{
-   return (
-     <div></div>
-   )
- }
-      
-    
+
+  }
+  else {
+    return (
+      <div></div>
+    )
+  }
+
+
 }
-   
+
 
 
 export default DishDetailComponent
